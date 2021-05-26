d86066e943116b9b974765db34d563f40ebd92d7
MinakshiMittal <49836499+MinakshiMittal@users.noreply.github.com>
2021-05-26 19:14:58 +0530

    feat: quiz category, score, analysis, more quiz

---
+++ src/Pages/QuizDetails.tsx
@@ -0,0 +1,13 @@
+import { QuizIntroduction } from "./QuizIntroduction";
+import { useParams } from "react-router-dom";
+import { mentalHealthQuizzes } from "../quiz-data";
+import { MentalHealthQuiz } from "../quiz-data.types";
+
+export const QuizDetails = () => {
+  const { quizId } = useParams();
+
+  const quizCategory: MentalHealthQuiz | undefined = mentalHealthQuizzes.find(
+    (quizCategory: MentalHealthQuiz) => quizCategory.id === quizId
+  );
+  return <QuizIntroduction quizCategory={quizCategory} />;
+};
---
+++ src/quiz-data.types.ts
@@ -0,0 +1,22 @@
+export type Option = {
+    optionId: string;
+    text: string;
+    isSelected: boolean;
+    points: number;
+  };
+  
+  export type QuizQuestion = {
+    questionId: string;
+    question: string;
+    options: Option[];
+  };
+  
+  export type MentalHealthQuiz = {
+    id: string;
+    quizName: string;
+    quizCoverImage: string;
+    questions: QuizQuestion[];
+  };
+  
+  export type MentalHealthQuizzes = MentalHealthQuiz[];
+  
